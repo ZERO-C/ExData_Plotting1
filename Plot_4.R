8877caa3977dd8203c2ff14454d3a56e2193ec25
ZERO-C <cangalaya1@gmail.com>
2014-12-03 14:04:49 -0500

    Update Plot_4.R
    Update

--- Plot_4.R
+++ Plot_4.R
@@ -22,15 +22,15 @@ working_data$datetime <- as.POSIXct(datetime)
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(working_data, {
 plot(Global_active_power~datetime, type="l",
-ylab="Global Active Power (kilowatts)", xlab="")
+ylab="Global Active Power", xlab="")
 
 #2 frame
 plot(Voltage~datetime, type="l",
-ylab="Voltage (volt)", xlab="")
+ylab="Voltage", xlab="datetime")
 
 #3 frame
 plot(Sub_metering_1~datetime, type="l",
-ylab="Global Active Power (kilowatts)", xlab="")
+ylab="Energy sub metering", xlab="")
 lines(Sub_metering_2~datetime,col='Red')
 lines(Sub_metering_3~datetime,col='Blue')
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
@@ -38,7 +38,7 @@ legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 #4 frame
 plot(Global_reactive_power~datetime, type="l",
-ylab="Global Rective Power (kilowatts)",xlab="")
+ylab="Global_reactive_power",xlab="datetime")
 
 })
 
