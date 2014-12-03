07c6c23ce263bef2add97c2e23ac66be87f2a05a
ZERO-C <cangalaya1@gmail.com>
2014-12-03 13:52:33 -0500

    Update Plot_4.R
    Update

--- Plot_4.R
+++ Plot_4.R
@@ -1,31 +1,48 @@
-## Getting dataset
+###### EXPLORATORY DATA ANALYSIS #########
+########## Peer Assessments 1 #############
+########### Course Project 1 ##############
+
+# Getting dataset
 getwd()
-setwd("C:/Users/Carla/Documents/ExData_Plotting1/")
-data_total <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",
-nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_total$Date <- as.Date(data_total$Date, format="%d/%m/%Y")
-## Subsetting the data
-data <- subset(data_total, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_total)
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-## Plot 4
+setwd("C:/Users/Administrador/Documents/ex_data")
+global_data <- read.csv("household_power_consumption.txt", header=T,sep=';', na.strings="?", nrow=2075259, check.names=F, stringsAsFactors=F,comment.char="", quote='\"')
+global_data$Date <- as.Date(global_data$Date, format="%d/%m/%Y")
+
+# Subsetting the data
+working_data<-subset(global_data,subset=(Date>="2007-02-01" & Date <="2007-02-02"))
+rm(global_data)
+
+# Converting dates
+datetime <- paste(as.Date(working_data$Date), working_data$Time)
+working_data$datetime <- as.POSIXct(datetime)
+
+# Plot 4 
+
+#1 frame
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
-with(data, {
-plot(Global_active_power~Datetime, type="l",
+with(working_data, {
+plot(Global_active_power~datetime, type="l",
 ylab="Global Active Power (kilowatts)", xlab="")
-plot(Voltage~Datetime, type="l",
+
+#2 frame
+plot(Voltage~datetime, type="l",
 ylab="Voltage (volt)", xlab="")
-plot(Sub_metering_1~Datetime, type="l",
+
+#3 frame
+plot(Sub_metering_1~datetime, type="l",
 ylab="Global Active Power (kilowatts)", xlab="")
-lines(Sub_metering_2~Datetime,col='Red')
-lines(Sub_metering_3~Datetime,col='Blue')
+lines(Sub_metering_2~datetime,col='Red')
+lines(Sub_metering_3~datetime,col='Blue')
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
 legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-plot(Global_reactive_power~Datetime, type="l",
+
+#4 frame
+plot(Global_reactive_power~datetime, type="l",
 ylab="Global Rective Power (kilowatts)",xlab="")
+
 })
-## Saving to file
+
+# Saving png format 
+
 dev.copy(png, file="plot4.png", height=480, width=480)
 dev.off()
