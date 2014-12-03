6316550301cf01603d58d560a4ceb426503e2912
ZERO-C <cangalaya1@gmail.com>
2014-12-03 13:55:40 -0500

    Update Plot_3.R
    Update

--- Plot_3.R
+++ Plot_3.R
@@ -18,6 +18,7 @@ working_data$datetime <- as.POSIXct(datetime)
 
 # Plot 3 
 
+par(mfrow=c(1,1))
 with(working_data, {
 plot(Sub_metering_1 ~ datetime, type="l",
 ylab="Global Active Power (kilowatts)", xlab="")
@@ -31,4 +32,3 @@ legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
-
