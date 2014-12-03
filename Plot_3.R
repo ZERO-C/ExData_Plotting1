d746f19fdd46c2f7946950565b8eea25ea11f959
ZERO-C <cangalaya1@gmail.com>
2014-12-03 13:39:32 -0500

    Update Plot_3.R
    Update

--- Plot_3.R
+++ Plot_3.R
@@ -1,24 +1,34 @@
-## Getting dataset
+###### EXPLORATORY DATA ANALYSIS #########
+########## Peer Assessments 1 #############
+########### Course Project 1 ##############
+
+# Getting dataset
 getwd()
-setwd("C:/Users/Carla/Documents/ExData_Plotting1/")
-data_total <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",
-nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_total$Date <- as.Date(data_total$Date, format="%d/%m/%Y")
-## Subsetting the data
-data <- subset(data_total, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_total)
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-## Plot 3
-with(data, {
-plot(Sub_metering_1~Datetime, type="l",
+setwd("C:/Users/Administrador/Documents/ex_data")
+global_data <- read.csv("household_power_consumption.txt", header=T,sep=';', na.strings="?", nrow=2075259, check.names=F, stringsAsFactors=F,comment.char="", quote='\"')
+global_data$Date <- as.Date(global_data$Date, format="%d/%m/%Y")
+
+# Subsetting the data
+working_data<-subset(global_data,subset=(Date>="2007-02-01" & Date <="2007-02-02"))
+rm(global_data)
+
+# Converting dates
+datetime <- paste(as.Date(working_data$Date), working_data$Time)
+working_data$datetime <- as.POSIXct(datetime)
+
+# Plot 3 
+
+with(working_data, {
+plot(Sub_metering_1 ~ datetime, type="l",
 ylab="Global Active Power (kilowatts)", xlab="")
-lines(Sub_metering_2~Datetime,col='Red')
-lines(Sub_metering_3~Datetime,col='Blue')
+lines(Sub_metering_2 ~ datetime,col='Red')
+lines(Sub_metering_3 ~ datetime,col='Blue')
 })
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2,
 legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-## Saving to file
+
+# Saving png format 
+
 dev.copy(png, file="plot3.png", height=480, width=480)
 dev.off()
+
