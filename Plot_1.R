916722fd40a2443999796ccfe60375157905a8b9
ZERO-C <cangalaya1@gmail.com>
2014-12-03 13:13:31 -0500

    Update Plot_1.R
    Update

--- Plot_1.R
+++ Plot_1.R
@@ -1,22 +1,31 @@
+###### EXPLORATORY DATA ANALYSIS #########
+########## Peer Assessments 1 #############
+########### Course Project 1 ##############
 
-## Getting dataset
+# Getting dataset
 getwd()
-setwd("C:/Users/Carla/Documents/ExData_Plotting1/")
-data_total <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",
-nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-data_total$Date <- as.Date(data_total$Date, format="%d/%m/%Y")
-## Subsetting the data
-data <- subset(data_total, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
-rm(data_total)
-## Converting dates
-datetime <- paste(as.Date(data$Date), data$Time)
-data$Datetime <- as.POSIXct(datetime)
-## Plot 1
-hist(data$Global_active_power, 
+setwd("C:/Users/Administrador/Documents/ex_data")
+global_data <- read.csv("household_power_consumption.txt", header=T,sep=';', na.strings="?", nrow=2075259, check.names=F, stringsAsFactors=F,comment.char="", quote='\"')
+global_data$Date <- as.Date(global_data$Date, format="%d/%m/%Y")
+
+# Subsetting the data
+working_data<-subset(global_data,subset=(Date>="2007-02-01" & Date <="2007-02-02"))
+rm(global_data)
+
+# Converting dates
+
+datetime <- paste(as.Date(working_data$Date), working_data$Time)
+working_data$datetime <- as.POSIXct(datetime)
+
+# Plot 1 (Basics)
+
+hist(working_data$Global_active_power, 
 main="Global Active Power",
 xlab="Global Active Power (kilowatts)", 
 ylab="Frequency", 
 col="Red")
-## Saving png format (view project instructions)
+
+# Saving png format 
+
 dev.copy(png, file="plot1.png", height=480, width=480)
 dev.off()
